f821eb837b47a53236ac437e3b6497849d47adbf
Daniel Fadamitan <104998213+Invalid8@users.noreply.github.com>
2024-07-30 21:22:53 +0100

    remove admin service

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -1,5 +1,18 @@
 import * as admin from "firebase-admin";
-import * as serviceAccount from "./service.json";
+
+const serviceAccount = {
+  type: import.meta.env.VITE_type,
+  project_id: import.meta.env.VITE_project_id,
+  private_key_id: import.meta.env.VITE_private_key_id,
+  private_key: import.meta.env.VITE_private_key,
+  client_email: import.meta.env.VITE_client_email,
+  client_id: import.meta.env.VITE_client_id,
+  auth_uri: import.meta.env.VITE_auth_uri,
+  token_uri: import.meta.env.VITE_token_uri,
+  auth_provider_x509_cert_url: import.meta.env.VITE_auth_provider_x509_cert_url,
+  client_x509_cert_url: import.meta.env.VITE_client_x509_cert_url,
+  universe_domain: import.meta.env.VITE_universe_domain,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
